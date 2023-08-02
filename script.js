26147a6836797dcd6c39a573658c9857847c27a2
sagarrao <sagarrao5537@gmail.com>
2023-08-02 15:31:40 +0530

    finished

---
+++ script.js
@@ -0,0 +1,89 @@
+const questions = [
+  {
+    que: "which of the following is a markup language ?",
+    a: "HTML",
+    b: "CSS",
+    c: "JavaScript",
+    d: "PHP",
+    correct: "a",
+  },
+  {
+    que: "What year was JavaScript launched ?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "none of the above",
+    correct: "b",
+  },
+  {
+    que: "what does CSS stands for ?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "Jason Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "b",
+  },
+];
+
+// == for loading questions (function) ==
+
+let index = 0;
+let total = questions.length;
+let right = 0,
+  wrong = 0;
+const quesBox = document.getElementById("quesBox");
+const optionInputs = document.querySelectorAll(".options");
+const loadQuestion = () => {
+  if (index === total) {
+    return endQuiz();
+  }
+  reset();
+  const data = questions[index];
+  // console.log(data);
+
+  quesBox.innerText = `(${index + 1}) ${data.que}`;
+
+  optionInputs[0].nextElementSibling.innerText = data.a;
+  optionInputs[1].nextElementSibling.innerText = data.b;
+  optionInputs[2].nextElementSibling.innerText = data.c;
+  optionInputs[3].nextElementSibling.innerText = data.d;
+};
+
+const submitQuiz = () => {
+  const data = questions[index];
+  const ans = getAnswer();
+  if (ans == data.correct) {
+    right++;
+  } else {
+    wrong++;
+  }
+  index++;
+  loadQuestion();
+  return;
+};
+
+const getAnswer = () => {
+  let answer;
+  optionInputs.forEach((input) => {
+    if (input.checked) {
+      answer = input.value;
+      // console.log("yes");
+    }
+  });
+  return answer;
+};
+
+const reset = () => {
+  optionInputs.forEach((input) => {
+    input.checked = false;
+  });
+};
+
+const endQuiz = () => {
+  document.getElementById("box").innerHTML = `<div style="text-align:center">
+  <h3>Thank You for participating in our Quiz</h3>
+       <h2>Right : ${right} Wrong : ${wrong}, total : ${total}  </h2>
+       </div>`;
+};
+// === initial call ===
+loadQuestion();
